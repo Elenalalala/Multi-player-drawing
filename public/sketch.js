35d765ab3bbf6fe563438595bb9f4cbe9dc9cf97
ElenaPeng <elenayue32486@gmail.com>
2021-03-24 16:17:15 +0800

    initial

---
+++ public/sketch.js
@@ -0,0 +1,80 @@
+let socket;
+
+
+let x=0;
+let y=100;
+let z=200;
+let colorValue =355;
+let pointerSize;
+let layer;
+let userInfo;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  colorValue = random(255,355);
+  pointerSize = random(20,50);
+
+  layer = createGraphics(windowWidth,windowHeight);
+
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse',newDrawing);
+  socket.on('user', newUserPointer);
+}
+
+function draw() {
+  background(255);
+  noFill();
+  stroke(noise(x)*colorValue,noise(y)*colorValue,noise(z)*colorValue);
+  ellipse(mouseX,mouseY,pointerSize,pointerSize);
+  text(socket.id,mouseX+20,mouseY);
+
+
+  //user cursor
+  if(userInfo != undefined){
+  ellipse(userInfo.x,userInfo.y,userInfo.size,userInfo.size);
+  text(userInfo.id,userInfo.x,userInfo.y);
+  }
+
+  image(layer,0,0);
+  if(mouseIsPressed){
+    x+=0.05;
+    y+=0.05;
+    z+=0.05;
+    layer.noStroke(); 
+    layer.fill(noise(x)*colorValue,noise(y)*colorValue,noise(z)*colorValue);
+    layer.ellipse(mouseX,mouseY, pointerSize,pointerSize);
+    
+    // console.log('Sending:'+mouseX+','+mouseY);
+    let data = {
+      x:mouseX,
+      y:mouseY,
+      r:noise(x)*colorValue,
+      g:noise(y)*colorValue,
+      b:noise(z)*colorValue,
+      size:pointerSize
+    };
+    socket.emit('mouse', data);
+  }
+  let userId = {
+    id:socket.id,
+    x:mouseX,
+    y:mouseY,
+    size:pointerSize
+  }
+  socket.emit('user', userId);
+}
+
+function newDrawing(data){
+  image(layer,0,0);
+  layer.noStroke(); 
+  layer.fill(data.r,data.g,data.b);
+  layer.ellipse(data.x,data.y, data.size,data.size);
+}
+function newUserPointer(info){
+  userInfo = info;
+  background(255);
+  noFill();
+  stroke(0);
+  
+}
+
---
+++ server.js
@@ -0,0 +1,27 @@
+let express = require('express'); // require returns a function;
+let app = express(); //then we call on this function, which will return all the things express give us, and put then in a variable for further reference.
+let server = app.listen(3000); // this will use one method inside of app (express()) and this will listen on the port 3000;
+
+app.use(express.static('public')); // this will host public folder as the static website
+console.log('my socket server is running');
+
+let socket = require('socket.io'); // require socket
+let io = socket(server);
+io.sockets.on('connection',(socket)=>{
+    console.log('new connection!' + socket.id);
+    socket.on('mouse', (data)=>{
+        socket.broadcast.emit('mouse',data); // you can alter the data here and then send it to broadcast. This will send to everyone else
+        // io.sockets.emit('mouse',data); //this will send to everyone including myself.
+        // console.log(data);
+    });
+    socket.on('user',(userId)=>{
+        // console.log(userId);
+        socket.broadcast.emit('user',userId);
+    });
+});
+
+
+// let serialserver = require('./node_modules/p5.serialserver/p5.serialserver.js');
+// serialserver.start(8081);
+
+// console.log("p5.serialserver is running");
